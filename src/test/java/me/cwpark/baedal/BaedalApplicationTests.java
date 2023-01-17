a78b208efc6bf0ced7c15e2556fa1e3daa1c9ada
cwjohnpark <pierrotcw@gmail.com>
2023-01-17 20:35:55 +0900

    init commit

---
+++ src/test/java/me/cwpark/baedal/BaedalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cwpark.baedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
