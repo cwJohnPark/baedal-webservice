03e7e27f34e4314e040dd488677b2cee78a70673
cwjohnpark <pierrotcw@gmail.com>
2023-01-24 09:13:44 +0900

    fix: Member email is unique

--- src/main/resources/db/migration/V1__Initialize_project_tables.sql
+++ src/main/resources/db/migration/V1__Initialize_project_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE member
 (
     id                 BIGINT NOT NULL AUTO_INCREMENT,
     name               VARCHAR(255) NOT NULL,
-    email              VARCHAR(255) NOT NULL,
+    email              VARCHAR(255) UNIQUE NOT NULL,
     password           VARCHAR(255) NOT NULL,
     created_at         TIMESTAMP NOT NULL,
     modified_at        TIMESTAMP,
@@ -23,3 +23,4 @@ CREATE TABLE delivery
 ALTER TABLE delivery
     ADD CONSTRAINT fk_delivery_member
         FOREIGN KEY (member_id) REFERENCES member (id);
+
